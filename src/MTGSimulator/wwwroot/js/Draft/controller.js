974865f0ebffbd5ef998e4688713708ad1d6af93
Nicolaj Enøe Simonsen <nicolaj-simonsen@hotmail.com>
2017-02-15 20:38:41 +0100

    Testcode

--- src/MTGSimulator/wwwroot/js/Draft/controller.js
+++ src/MTGSimulator/wwwroot/js/Draft/controller.js
@@ -6,6 +6,14 @@
 
         function onConnecting() {
             console.log("Connected");
+            draftHub.invoke("CreateDraft", ["LEA"], function() {});
         }
+
+        draftHub.on("InitializeGame",
+            function(response) {
+                console.log(response.draftId);
+                console.log(response.playerId);
+                console.log(response.boosters);
+            });
     }
 ]);
